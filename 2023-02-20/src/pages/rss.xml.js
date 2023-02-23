d2f0dbaa7bd6502cd4a501ed9709f14b8255de82
Nao <maroon.nopia@gmail.com>
2023-02-23 14:23:10 +0900

    [del]

--- 2023-02-20/src/pages/rss.xml.js
+++
@@ -1,8 +0,0 @@
-import rss from '@astrojs/rss';
-
-export const get = () => rss({
-        title: "n-ao's blog",
-        description: "ブログです",
-        site: import.meta.env.SITE,
-        items: import.meta.glob('./**/*md'),
-    });
